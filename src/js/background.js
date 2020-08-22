99833150822b219cc3f4a811557482955914beb3
Raphael Bernhart <mail@raphaelbernhart.at>
2020-08-23 00:33:10 +0200

    added first-run page

---
+++ src/js/background.js
@@ -0,0 +1,10 @@
+chrome.runtime.onInstalled.addListener(function() {
+    chrome.storage.sync.set({color: '#3aa757'}, function() {
+        console.log("The color is green.");
+    });
+
+    chrome.tabs.create({
+        active: true,
+        url: "first-run.html"
+    });
+});
